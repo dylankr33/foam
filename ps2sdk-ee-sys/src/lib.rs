9c1fb34007795552862f77c6e466cd67cc3d1fd3
dylankr33 <dylanjkr@proton.me>
2026-03-30 19:08:04 -0400

    delete ps2

--- ps2sdk-ee-sys/src/lib.rs
+++
@@ -1,8 +0,0 @@
-#![allow(non_upper_case_globals)]
-#![allow(non_camel_case_types)]
-#![allow(non_snake_case)]
-#![no_std]
-
-use core::{concat, env, include, prelude::rust_2024::*};
-
-include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
--- ps2sdk-ee-sys/wrapper.h
+++
@@ -1,6 +0,0 @@
-#include <erl.h>
-#include <draw.h>
-#include <draw3d.h>
-#include <graph.h>
-#include <stdio.h>
-#include <stdlib.h>
